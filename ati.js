7181d0362e768ebe4ec816d47f2000578e89d1fb
Yves Van Goethem <yves@soundcloud.com>
2012-11-06 16:15:50 +0100

    - Refactor ATI library   - Moved private functions out of the public interface   - Got rid of jQuery dependencies - Now supports page views, audio support still in progress - Introduces a mandatory initialization method

--- ati.js
+++ ati.js
@@ -1,199 +1,353 @@
 /**
- * This file provides a localStorage wrapper for AT Internet click events
+ * This file provides an interface that communicates with ATI's endpoints and queues the requests.
  */
 
 /**
- * @license
- *
- * Copyright (c) 2012, SoundCloud Ltd., Yves Van Goethem
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
- *
- *   * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+* @license
+*
+* Copyright (c) 2012, SoundCloud Ltd., Yves Van Goethem
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+*
+* * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+* * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
-ATI = {
+(function(){
+
+  var ATI,
+
+    /* local vars */
+    initialized, siteId, subdomain,
+
+    /* utilities helpers */
+    isObjectEmpty, isArray,
+    supportsLocalStorage, supportsJSON,
+
+    /* formatting helpers */
+    getDomain, getFormatedTime, getFormatedScreenProperties, getScreenSize, getCombinedURL,
+
+    /* storage helpers */
+    getStorage, setStorage, processStorage, resetStorage, sendData,
+
+    /**
+     * The name of the localStorage key we use
+     * @type {String}
+     */
+    STORAGE_KEY = 'sc-ati-events-test';
+
+  ATI = {
+
+    /**
+     * Initializes the wrapper
+     * @param {Object} params           Contains the mandatory initialization params
+     * @param {Number} params.id        The ATI site ID
+     * @param {String} params.subdomain The ATI subdomain, prefer the SSL variation.
+     * @example
+     *   ATI.init({
+     *     id: 123456,
+     *     subdomain: 'https://logs1234'
+     *   });
+     * @see ATI Documentation for more details about your Site ID and your subdomain.
+     * @final
+     */
+    init: function(params) {
+      var error;
+
+      if (initialized) {
+        error = 'Already initialized';
+      } else if (!params || (params && (!params.id || !params.subdomain))) {
+        error = 'Site id and subdomain are missing as parameters';
+      }
+
+      if (error) {
+        throw new Error('ATI: ' + error);
+      }
+
+      if (supportsLocalStorage() && supportsJSON()) {
+        siteId = params.id;
+        subdomain = params.subdomain;
+        if (getStorage() && getStorage().length) {
+          processStorage();
+        } else {
+          emptyStorage();
+        }
+        initialized = true;
+      }
+    },
+
+    /**
+     * Pushes data to the storage queue
+     * @param {Object}   event          The data and options to send to ATI
+     * @param {String}   event.page     The page 'foo' or as a chapter 'foo::bar'
+     * @param {String=}  event.type     Defines the event type, defaults to 'A' for click
+     * @param {String=}  event.level    Second Level ID, see ATI documentation for more details
+     * @param {String=}  event.action   For audio events only: 'play'|'pause'|'refresh'
+     * @param {String=}  event.duration For audio events only, in milliseconds
+     * @example
+     *   ATI.push({
+     *     page  : 'foo::bar',
+     *     type  : 'A',
+     *     level : '1'
+     *   });
+     * @see ATI Documentation for more details about the event properties and values
+     * @final
+     */
+    push: function(event) {
+      var storage, events, uuid, newEvent, error;
+
+      if (!initialized) {
+        error = 'you need to initialize the library first';
+      } else if (typeof event !== 'object') {
+        error = 'only objects are accepted';
+      } else if (!event.page) {
+        error = 'page is missing in event';
+      } else if (event.type === 'audio' && !event.action) {
+        error = 'action is missing in event';
+      }
+
+      if (error) {
+        throw new Error('ATI: ' + error);
+      }
+
+      /* if no level is passed it defaults to an empty string */
+      event.level = event.level || '';
+
+      storage = getStorage(),
+      events  = storage ? storage : [],
+      uuid    = new Date().getTime();
+
+      if (!isArray(events)) {
+        events = [ events ];
+      }
+
+      newEvent = {};
+      newEvent[uuid] = event;
+      events.push(newEvent);
+      setStorage(events);
+      processStorage();
+    }
+
+  };
 
   /**
-   * The name of the localStorage item we set
-   * @type {String}
+   * Checks if the passed object isn't empty
+   * @param {Object} obj
+   * @return {Boolean}
    */
-  name: 'ati-events',
+  isObjectEmpty = function(obj) {
+    for (var key in obj) {
+      return false;
+    }
+    return true;
+  };
 
   /**
-   * Store data or reset the storage when nothing is passed
+   * Checks if the passed object is an Array
+   * @param {Object} arr
+   * @return {Boolean}
+   */
+  isArray = function(arr) {
+    return Object.prototype.toString.call(arr) === '[object Array]';
+  };
+
+  /**
+   * Checks if the browser supports localStorage
+   * @return {Boolean}
+   */
+  supportsLocalStorage = function() {
+    var supportsLocalStorage = true;
+    try {
+      window.localStorage.setItem('sc-test-storage','1');
+      window.localStorage.removeItem('sc-test-storage');
+    } catch (error) {
+      supportsLocalStorage = false;
+    }
+    return supportsLocalStorage;
+  };
+
+  /**
+   * Checks if the browser supports JSON
+   * @return {Boolean}
+   */
+  supportsJSON = function() {
+    return typeof window.JSON !== 'undefined';
+  };
+
+  /**
+   * Combines specific subdomain with full ATI URL
+   * @return {String}
+   */
+  getDomain = function() {
+    return subdomain + '.xiti.com/hit.xiti?';
+  };
+
+  /**
+   * Returns a specifically formated string for ATI of the local time.
+   * @return {String}
+   */
+  getFormatedTime = function() {
+    var date = new Date();
+    return [date.getHours(), date.getMinutes(), date.getSeconds()].join('x');
+  };
+
+  /**
+   * Returns a specifically formated string for ATI of some screen properties
+   * @return {String}
+   */
+  getFormatedScreenProperties = function() {
+    return [window.screen.availWidth, window.screen.availHeight, window.screen.pixelDepth, window.screen.colorDepth].join('x');
+  };
+
+  /**
+   * Returns a specifically formated string for ATI of screen resolution
+   * @return {String}
+   */
+  getFormatedScreenSize = function() {
+    return [window.screen.width, window.screen.height].join('x');
+  };
+
+  /**
+   * Returns a valid URL for ATI's API by combining multiple query strings depending on the event type
+   * @param  {Object} event The event that was passed by processStorage
+   * @return {String} The URL to perform a GET request
+   */
+  getCombinedURL = function(event) {
+    var url = getDomain();
+
+    /* Basics for all URLs */
+    url += [
+      's='   + siteId,
+      's2='  + event.level,
+      'p='   + event.page,
+      'r='   + getFormatedScreenProperties(),
+      're='  + getFormatedScreenSize(),
+      'hl='  + getFormatedTime(),
+      'jv='  + (navigator.javaEnabled() ? 1 : 0),
+      'lng=' + navigator.language
+    ].join('&');
+
+    switch (event.type) {
+
+      /* if no type is defined it counts as a page view */
+      case undefined:
+        url += '&ref=' + document.referrer;
+        break;
+
+      /* In case of a click — the type 'A' defines a click, yup. And allows for any other type like 'N'… see the doc */
+      case 'A':
+        url += '&clic=' + event.type;
+        break;
+
+      case 'audio':
+        url += [
+          'type='   + event.type,
+          'a='      + event.action,
+          'm1='     + event.duration,
+          'm5=int',   /* We don't need to change that, all of our plays are internal */
+          'm6=clip',  /* We don't need to change that, we always know the duration of the sound */
+          'prich=',   // TODO
+          's2rich='   // TODO
+        ].join('&');
+        break;
+
+    }
+
+    return url;
+  };
+
+  /**
+   * Stores data in the storage
+   * When nothing is passed it resets the storage, call emptyStorage() to do so explicitly.
    * @param {String=} data String to store in localStorage
    */
-  setStorage: function(data) {
-    // make sure to get rid of empty objects
+  setStorage = function(data) {
     if (data) {
-      $.each(data, function(i, item) {
-        if ($.isEmptyObject(item)) {
+      var i = 0;
+      for (var item in data) {
+        if (isObjectEmpty(item)) {
           if (data.length > 1) {
             data = data.splice(i, 1);
           } else {
             data = data.splice(0, 0);
           }
         }
-      });
+        i++;
+      }
     }
-    window.localStorage.setItem(this.name, JSON.stringify(data || []));
-  },
+    window.localStorage.setItem(STORAGE_KEY, JSON.stringify(data || []));
+  };
 
   /**
    * Returns a stringified object of the storage
    * @return {String}
    */
-  getStorage: function() {
-    return JSON.parse(window.localStorage.getItem(this.name));
-  },
+  getStorage = function() {
+    return JSON.parse(window.localStorage.getItem(STORAGE_KEY));
+  };
 
   /**
-   * Used by hasLocalStorage to store the result of initial storage access and prevent unecessary access
-   * @private {boolean}
+   * Processes the storage queue
    */
-  _hasLocalStorage: null,
-
-  /**
-   * Verifies if the browser supports localStorage
-   * @return {boolean}
-   */
-  hasLocalStorage: function() {
-    if (this._hasLocalStorage !== null) {
-      return this._hasLocalStorage;
-    }
-    var hasLocalStorage = true;
-    try {
-      window.localStorage.setItem('sc-test-storage','1');
-      window.localStorage.removeItem('sc-test-storage');
-    } catch (error) {
-      hasLocalStorage = false;
-    }
-    this._hasLocalStorage = hasLocalStorage;
-    return hasLocalStorage;
-  },
-
-  /**
-   * Initializes the wrapper
-   * @final
-   */
-  init: function() {
-    if (this.hasLocalStorage()) {
-      this.setup();
-    }
-  },
-
-  /**
-   * Runs necessary events and functions after initialization
-   */
-  setup: function() {
-    // content available? process it!
-    if (this.getStorage() && this.getStorage().length) {
-      this.process();
-    } else {
-      this.setStorage();
-    }
-  },
-
-  /**
-   * Procces the storage queue
-   */
-  process: function() {
-    var events = this.getStorage(),
-        that = this;
-    $.each(events, function(i, event) {
+  processStorage = function() {
+    var events = getStorage();
+    for (var event in events) {
       var uuid;
-      for (var key in this) {
+      for (var key in events[event]) {
         uuid = key;
       }
-      that.request(event[uuid], uuid);
-    });
-  },
+      sendData(events[0][uuid], uuid);
+    }
+  };
 
   /**
-   * Pushes data to the storage queue
-   * @param {{page: String, type: String, level: String=}} event
-   * @example
-   *   ATI.push({
-   *     page  : 'foo::bar',
-   *     level : '1',
-   *     type  : 'A'
-   *   });
+   * Empties the storage
    */
-  push: function(event) {
-    if (!this.hasLocalStorage()) {
+  emptyStorage = function() {
+    window.localStorage.setItem(STORAGE_KEY, JSON.stringify([]));
+  };
+
+  /**
+   * Sends the values to ATI
+   * @param  {Object} event
+   * @param  {String} uuid
+   */
+  sendData = function(event, uuid) {
+    if (isObjectEmpty(event)) {
+      emptyStorage();
       return;
     }
-    if (typeof event !== 'object') {
-      throw new Error('ATI: Only objects are acceptable');
-    }
-    if (!event.page) {
-      throw new Error('ATI: Page is missing in event');
-    }
-    if (!event.type) {
-      throw new Error('ATI: Type is missing in event');
-    }
-    if (!window.xtsite) {
-      throw new Error('ATI: window.xtsite is missing, check your maintag!');
-    }
-    if (!window.xtsd) {
-      throw new Error('ATI: window.xtsd is missing, check your maintag!');
-    }
 
-    var stored = this.getStorage(),
-        events = stored ? stored : [],
-        uuid   = new Date().getTime();
-
-    if (!$.isArray(events)) {
-      events = [ events ];
-    }
-    var newEvent = {};
-    newEvent[uuid] = event;
-    events.push(newEvent);
-    this.setStorage(events);
-    this.process();
-  },
-
-  /**
-   * Do a request to ATI servers
-   */
-  request: function(event, uuid) {
-    var that   = this,
-        $img   = $(new Image()),
-        date   = new Date(),
-        url    = window.xtsd + '.xiti.com/hit.xiti',
-        time   = date.getHours() + 'x' + date.getMinutes() + 'x' + date.getSeconds(),
-        screen = window.screen.width + 'x' + window.screen.height + 'x' + window.screen.pixelDepth + 'x' + window.screen.colorDepth,
-        level  = event.level || '';
-    $img
-      .attr('src', url + '?s=' + window.xtsite + '&s2=' + level + '&p=' + event.page + '&clic=' + event.type + '&hl=' + time + '&r=' + screen)
-      .attr('data-ati-uuid', uuid)
-      // should probably be a className?
-      .css({
-        width:'1px',
-        height:'1px',
-        position:'absolute',
-        left:'-9999em'
-      })
-      // make sure the request was done before removing it from the storage
-      .bind('load', function() {
-        var stored = that.getStorage();
-        for (var key in stored) {
-          var item = stored[key];
-          for (var id in item) {
-            if ($(this).attr('data-ati-uuid') === id) {
-              delete item[id];
-              that.setStorage(stored);
-              $img.remove();
-              break;
+    var img = new Image();
+    img.onload = function() {
+      var storage = getStorage();
+      for (var key in storage) {
+        var item = storage[key];
+        for (var id in item) {
+          if (this.getAttribute('data-ati-uuid') === id) {
+            delete item[id];
+            if (isObjectEmpty(storage[0])) {
+              emptyStorage();
+            } else {
+              setStorage(storage);
             }
+            break;
           }
         }
-      });
-    $('body').append($img);
+      }
+    };
+    img.setAttribute('data-ati-uuid', uuid);
+    img.src = getCombinedURL(event);
+  };
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = ATI;
+  } else {
+    window.SC = window.SC || {};
+    window.SC.ATI = ATI;
   }
-};
+
+})();
