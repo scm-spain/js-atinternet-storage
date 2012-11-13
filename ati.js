027891ba66c9e9d0923a64c8858b512e64b5bc55
Yves Van Goethem <yves@soundcloud.com>
2012-11-13 16:02:17 +0100

    Changed public interface, supports audio events, jshint valid & minor bug fixes

--- ati.js
+++ ati.js
@@ -1,3 +1,4 @@
+/*jshint forin:false bitwise:false*/
 /**
  * This file provides an interface that communicates with ATI's endpoints and queues the requests.
  */
@@ -21,124 +22,134 @@
   var ATI,
 
     /* local vars */
-    initialized, siteId, subdomain,
+    initialized, siteId, subdomain, customVariables, _customVariables,
 
     /* utilities helpers */
     isObjectEmpty, isArray,
     supportsLocalStorage, supportsJSON,
+    generateUUID, passesConditions,
 
     /* formatting helpers */
-    getDomain, getFormattedTime, getFormattedScreenProperties, getScreenSize, getCombinedURL,
+    getDomain, getFormattedTime, getFormattedScreenProperties, getFormattedScreenSize, getCombinedURL,
+
+    /* data helpers */
+    processEvent, pushEvent, sendData,
 
     /* storage helpers */
-    getStorage, setStorage, processStorage, resetStorage, sendData,
+    getStorage, setStorage, processStorage, emptyStorage,
 
     /**
      * The name of the localStorage key we use
      * @type {String}
      */
-    STORAGE_KEY = 'sc-ati-events-test',
-
-    /**
-     * Increases with each push and acts as a UUID
-     * @type {Number}
-     */
-    counter = 0;
+    STORAGE_KEY = 'sc-ati-events-test';
 
   ATI = {
 
     /**
      * Initializes the wrapper
      * @param {Object} params           Contains the mandatory initialization params
-     * @param {Number} params.id        The ATI site ID
+     * @param {String} params.id        The ATI site ID
      * @param {String} params.subdomain The ATI subdomain, prefer the SSL variation.
      * @example
      *   ATI.init({
-     *     id: 123456,
+     *     id: '123456',
      *     subdomain: 'https://logs1234'
      *   });
-     * @see ATI Documentation for more details about your Site ID and your subdomain.
+     * See ATI Documentation for more details about your Site ID and your subdomain.
      * @final
      */
-    init: function(params) {
-      var error, storage;
+    initialize: function(params) {
+      if (passesConditions('initialize', params)) {
+        if (supportsLocalStorage() && supportsJSON()) {
+          var storage = getStorage();
+          siteId          = params.id;
+          subdomain       = params.subdomain;
+          customVariables = {};
 
-      if (initialized) {
-        error = 'Already initialized';
-      } else if (!params || !params.id || !params.subdomain) {
-        error = 'Site id and subdomain are missing as parameters';
-      }
+          if (storage && storage.length) {
+            processStorage();
+          } else {
+            emptyStorage();
+          }
 
-      if (error) {
-        throw new Error('ATI: ' + error);
-      }
-
-      if (supportsLocalStorage() && supportsJSON()) {
-        siteId    = params.id;
-        subdomain = params.subdomain;
-        storage   = getStorage();
-
-        if (storage && storage.length) {
-          processStorage();
-        } else {
-          emptyStorage();
+          initialized = true;
         }
-
-        initialized = true;
       }
     },
 
     /**
-     * Pushes data to the storage queue
-     * @param {Object}   event          The data and options to send to ATI
-     * @param {String}   event.page     The page 'foo' or as a chapter 'foo::bar'
-     * @param {String=}  event.type     Defines the event type, defaults to 'A' for click
-     * @param {String=}  event.level    Second Level ID, see ATI documentation for more details
-     * @param {String=}  event.action   For audio events only: 'play'|'pause'|'refresh'
-     * @param {String=}  event.duration For audio events only, in milliseconds
-     * @example
-     *   ATI.push({
-     *     page  : 'foo::bar',
-     *     type  : 'A',
-     *     level : '1'
+     * Triggers an audio event
+     * @param {Object}  params
+     * @param {String}  params.page         The playing sound's page (yours or a users)
+     * @param {String}  params.level        The playing sound's page level (yours or a users)
+     * @param {String}  params.action       'play'|'pause'|'refresh'
+     * @param {String}  params.duration     in seconds
+     * @param {String}  params.contextPage  Page where the play happened (e.g. stream)
+     * @param {String}  params.contextLevel Second Level ID where the play happened (e.g. stream)
+     * @param {String?} params.qualityID    We use the quality ID as an option.
+     * @example to trigger an audio event from the stream page
+     *   ATI.triggerAudioEvent({
+     *     page         : 'sound::http://soundcloud.com/yvg/sound-xyz',
+     *     level        : '7',
+     *     action       : 'play',
+     *     duration     : '123456',
+     *     contextPage  : 'sound::http://soundcloud.com/stream',
+     *     contextLevel : '1'
      *   });
-     * @see ATI Documentation for more details about the event properties and values
      * @final
      */
-    push: function(event) {
-      var events, uuid, newEvent, error;
+    triggerAudioEvent: function(params) {
+      processEvent('audio', params);
+    },
 
-      if (!initialized) {
-        error = 'the library needs to be initialized first';
-      } else if (typeof event !== 'object') {
-        error = 'only objects are accepted';
-      } else if (!event.page) {
-        error = 'page is missing in event';
-      } else if (event.type === 'audio' && !event.action) {
-        error = 'action is missing in event';
+    /**
+     * Triggers a custom event, like a click.
+     * @param {Object} params
+     * @param {String} params.type  'A' for a click (doesn't support others for now)
+     * @param {String} params.page  The page 'http://soundcloud.com/bar' or as a chapter 'foo::http://soundcloud.com/bar'
+     * @param {String} params.level Second Level ID, see ATI documentation for more details
+     * @example to trigger a click event on the "repost dashbox" from the stream
+     *   ATI.triggerCustomEvent({
+     *     type  : 'A',
+     *     page  : 'dashbox::click::repost::http://soundcloud.com/stream',
+     *     level : '1'
+     *   });
+     * @final
+     */
+    triggerCustomEvent: function(params) {
+      processEvent('custom', params);
+    },
+
+    /**
+     * Triggers a page view event.
+     * @param {Object} params
+     * @param {String} params.page  The page 'http://soundcloud.com/bar' or as a chapter 'foo::http://soundcloud.com/bar'
+     * @param {String} params.level Second Level ID, see ATI documentation for more details
+     * @example to trigger a page view event of the stream
+     *   ATI.triggerPageView({
+     *     page  : 'http://soundcloud.com/stream',
+     *     level : '1'
+     *   });
+     * @final
+     */
+    triggerPageView: function(params) {
+      processEvent('pageview', params);
+    },
+
+    /**
+     * Sets custom variables that are later sent with a pageview, all of them.
+     * @param {Object} variables
+     * @example to set the users plan:
+     * ATI.setCustomVariables({
+     *   xt_ac: 4
+     * });
+     */
+    setCustomVariables: function(variables) {
+      for (var key in variables) {
+        customVariables[key] = variables[key];
       }
-
-      if (error) {
-        throw new Error('ATI: ' + error);
-      }
-
-      /* if no level is passed it defaults to an empty string */
-      event.level = event.level || '';
-
-      events = getStorage() || [],
-      uuid = ++counter;
-
-      if (!isArray(events)) {
-        events = [ events ];
-      }
-
-      newEvent = {};
-      newEvent[uuid] = event;
-      events.push(newEvent);
-      setStorage(events);
-      processStorage();
     }
-
   };
 
   /**
@@ -186,6 +197,58 @@
   };
 
   /**
+   * Generates an RFC4122 4.0 compliant UUID
+   * See http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript/2117523#2117523
+   * @return {String}
+   */
+  generateUUID = function() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
+      return v.toString(16);
+    });
+  };
+
+  /**
+   * Returns an error if one condition does not pass otherwise returns true
+   * @param  {String} type
+   * @param  {Object} params Contains params to check against
+   * @return {Boolean?}
+   */
+  passesConditions = function(type, params) {
+    var error, rules, paramsErrorStr;
+    paramsErrorStr = 'One or multiple params are missing';
+
+    if (type === 'initialize') {
+      if (initialized) {
+        error = 'Already initialized';
+      } else if (!params || !params.id || !params.subdomain) {
+        error = paramsErrorStr;
+      }
+    } else if (type === 'audio') {
+      if (!params || !params.page || !params.level || !params.action ||
+          !params.duration || !params.contextPage || !params.contextLevel) {
+        error = paramsErrorStr;
+      }
+    } else if (type === 'custom') {
+      if (!params || !params.page || !params.level || !params.type) {
+        error = paramsErrorStr;
+      }
+    } else if (type === 'pageview') {
+      if (!params || !params.page || !params.level) {
+        error = paramsErrorStr;
+      }
+    } else if (!initialized) {
+      error = 'the library needs to be initialized first';
+    }
+
+    if (error) {
+      throw new Error('ATI: ' + error);
+    } else {
+      return true;
+    }
+  };
+
+  /**
    * Combines specific subdomain with full ATI URL
    * @return {String}
    */
@@ -195,11 +258,24 @@
 
   /**
    * Returns a specifically formatted string for ATI of the local time.
-   * @return {String}
+   * @return {String} hhxmmxss
    */
   getFormattedTime = function() {
-    var date = new Date();
-    return [date.getHours(), date.getMinutes(), date.getSeconds()].join('x');
+    var date  = new Date(),
+        units = [date.getHours(), date.getMinutes(), date.getSeconds()],
+        time  = [],
+        unit, i, l;
+
+    for (i = 0, l = units.length; i < l; i++) {
+      unit = String(units[i]);
+      if (unit.length === 1) {
+        time.push('0' + unit);
+      } else {
+        time.push(unit);
+      }
+    }
+
+    return time.join('x');
   };
 
   /**
@@ -224,7 +300,8 @@
    * @return {String} The URL to perform a GET request
    */
   getCombinedURL = function(event) {
-    var url = getDomain();
+    var url = getDomain(),
+        key;
 
     /* Basics for all URLs */
     url += [
@@ -238,30 +315,30 @@
       'lng=' + navigator.language
     ].join('&');
 
-    switch (event.type) {
-
-      /* if no type is defined it counts as a page view */
-      case undefined:
-        url += '&ref=' + document.referrer;
-        break;
-
-      /* In case of a click — the type 'A' defines a click, yup. And allows for any other type like 'N'… see the doc */
-      case 'A':
-        url += '&clic=' + event.type;
-        break;
-
-      case 'audio':
-        url += [
-          'type='   + event.type,
-          'a='      + event.action,
-          'm1='     + event.duration,
-          'm5=int',   /* We don't need to change that, all of our plays are internal */
-          'm6=clip',  /* We don't need to change that, we always know the duration of the sound */
-          'prich=',   // TODO
-          's2rich='   // TODO
-        ].join('&');
-        break;
-
+    if (event.type) {
+      /* Custom events, like clicks. */
+      url += '&clic=' + event.type;
+    } else if (event.action && event.duration) {
+      /* Audio events */
+      url += [
+        '&type=audio',
+        'a='      + event.action,
+        'm1='     + event.duration,
+        'm5=int',   /* We don't need to change that, all of our plays are internal */
+        'm6=clip',  /* We don't need to change that, we always know the duration of the sound */
+        'prich='  + event.contextPage,
+        's2rich=' + event.contextLevel,
+        'm3='     + event.qualityID
+      ].join('&');
+    } else {
+      /* else it counts as a pageview */
+      url += '&ref=' + document.referrer;
+      if (_customVariables !== customVariables) {
+        for (key in customVariables) {
+          url += '&' + key + '=' + customVariables[key];
+        }
+        _customVariables = customVariables;
+      }
     }
 
     return url;
@@ -299,13 +376,13 @@
    * Processes the storage queue
    */
   processStorage = function() {
-    var events = getStorage();
-    for (var i = 0, l = events.length; i < l; ++i) {
-      var uuid;
-      for (var key in events[i]) {
+    var events, i, l, key, uuid;
+    events = getStorage();
+    for (i = 0, l = events.length; i < l; ++i) {
+      for (key in events[i]) {
         uuid = key;
       }
-      sendData(events[0][uuid], uuid);
+      sendData(events[i][uuid], uuid);
     }
   };
 
@@ -314,6 +391,37 @@
    */
   emptyStorage = function() {
     window.localStorage.setItem(STORAGE_KEY, JSON.stringify([]));
+  };
+
+  /**
+   * Processes the event if all parameters were passed
+   * @param  {String} type
+   * @param  {Object} params Contains params to check against
+   */
+  processEvent = function(type, params) {
+    if (passesConditions(type, params)) {
+      pushEvent(params);
+    }
+  };
+
+  /**
+   * Pushes the event to the localStorage and processes it
+   * @param {Object} event The params coming from a public method
+   */
+  pushEvent = function(event) {
+    var events, uuid, newEvent;
+
+    events = getStorage() || [],
+    uuid = generateUUID();
+    if (!isArray(events)) {
+      events = [ events ];
+    }
+
+    newEvent = {};
+    newEvent[uuid] = event;
+    events.push(newEvent);
+    setStorage(events);
+    processStorage();
   };
 
   /**
@@ -329,10 +437,11 @@
 
     var img = new Image();
     img.onload = function() {
-      var storage = getStorage();
-      for (var i = 0, l = storage.length; i < l; ++i) {
-        var item = storage[i];
-        for (var id in item) {
+      var storage = getStorage(),
+          i, l, item, id;
+      for (i = 0, l = storage.length; i < l; ++i) {
+        item = storage[i];
+        for (id in item) {
           if (this['data-ati-uuid'] === id) {
             delete item[id];
             if (isObjectEmpty(storage[0])) {
@@ -356,4 +465,4 @@
     window.SC.ATI = ATI;
   }
 
-})();
+}());
